c81e5a264f9cddbd254c9cb0b07a560b7e75bf85
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2017-02-08 01:31:28 -0500

    more updates

--- js/bulma.js
+++ js/bulma.js
@@ -1,10 +1,45 @@
-// The following code is based off a toggle menu by @Bradcomp
-// source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
-(function() {
-    var burger = document.querySelector('.nav-toggle');
-    var menu = document.querySelector('.nav-menu');
-    burger.addEventListener('click', function() {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
-    });
-})();
+removeScroll();
+$(document).ready(function () {
+	$("#home").click(function () {
+		$("#home").addClass('is-active');
+		$("#projects").removeClass('is-active');
+		$("#contact").removeClass('is-active');
+		goHome();
+	});
+	$("#projects").click(function () {
+		$("#projects").addClass('is-active');
+		$("#home").removeClass('is-active');
+		$("#contact").removeClass('is-active');
+		goProjects();
+	});
+	$("#contact").click(function () {
+		$("#contact").addClass('is-active');
+		$("#projects").removeClass('is-active');
+		$("#home").removeClass('is-active');
+		goContact();
+	});
+});
+
+function removeScroll() {
+	$("html, #page").addClass("scrollhid");
+	var page = $("#page").get();
+	$("page").css("paddingRight", page.offsetWidth - page.clientWidth + "px");
+}
+
+function goHome() {
+	console.log("home");
+	var body = $("#body");
+	body.replaceWith("<div id=\"body\" class=\"container has-text-centered\"><h1 class=\"title is-2\">Saejin Mahlau-Heinert</h1></div>");
+}
+
+function goProjects() {
+	console.log("projects");
+	var body = $("#body");
+	body.replaceWith("<div id=\"body\" class=\"container has-text-centered\"><h1 class=\"title is-2\">Projects</h1></div>");
+}
+
+function goContact() {
+	console.log("contact");
+	var body = $("#body");
+	body.replaceWith("<div id=\"body\" class=\"container has-text-centered\"><h1 class=\"title is-2\">Contact me at saejinmh@gmail.com</h1></div>");
+}
