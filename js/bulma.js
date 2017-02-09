9632966e5f6daef1b192a8aac7f328c6be4ea136
Saejin Mahlau-Heinert <mahlauheinerts@allegheny.edu>
2017-02-09 13:36:54 -0500

    updates

--- js/bulma.js
+++ js/bulma.js
@@ -1,21 +1,12 @@
 removeScroll();
 $(document).ready(function () {
 	$("#home").click(function () {
-		$("#home").addClass('is-active');
-		$("#projects").removeClass('is-active');
-		$("#contact").removeClass('is-active');
 		goHome();
 	});
 	$("#projects").click(function () {
-		$("#projects").addClass('is-active');
-		$("#home").removeClass('is-active');
-		$("#contact").removeClass('is-active');
 		goProjects();
 	});
 	$("#contact").click(function () {
-		$("#contact").addClass('is-active');
-		$("#projects").removeClass('is-active');
-		$("#home").removeClass('is-active');
 		goContact();
 	});
 });
@@ -28,18 +19,33 @@ function removeScroll() {
 
 function goHome() {
 	console.log("home");
-	var body = $("#body");
-	body.replaceWith("<div id=\"body\" class=\"container has-text-centered\"><h1 class=\"title is-2\">Saejin Mahlau-Heinert</h1></div>");
+	$("#home").addClass('is-active');
+	$("#projects").removeClass('is-active');
+	$("#contact").removeClass('is-active');
+
+	$("#body").replaceWith("<div id=\"body\" class=\"container has-text-centered\"><h1 class=\"title is-2\">Saejin Mahlau-Heinert</h1></div>");
 }
 
 function goProjects() {
 	console.log("projects");
-	var body = $("#body");
-	body.replaceWith("<div id=\"body\" class=\"container has-text-centered\"><h1 class=\"title is-2\">Projects</h1></div>");
+	$("#projects").addClass('is-active');
+	$("#home").removeClass('is-active');
+	$("#contact").removeClass('is-active');
+	$.ajax({
+			url: 'html/projects',
+			dataType: 'html',
+			success: function(data) {
+				alert(data);
+			}
+		});
+	$("#body").replaceWith("<div id=\"body\" class=\"container has-text-centered\"><h1 class=\"title is-2\">Projects</h1></div>");
 }
 
 function goContact() {
 	console.log("contact");
-	var body = $("#body");
-	body.replaceWith("<div id=\"body\" class=\"container has-text-centered\"><h1 class=\"title is-2\">Contact me at saejinmh@gmail.com</h1></div>");
+	$("#contact").addClass('is-active');
+	$("#projects").removeClass('is-active');
+	$("#home").removeClass('is-active');
+
+	$("#body").replaceWith("<div id=\"body\" class=\"container has-text-centered\"><h1 class=\"title is-2\">Contact me at saejinmh@gmail.com</h1></div>");
 }
